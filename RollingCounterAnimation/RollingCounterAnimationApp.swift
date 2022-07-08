a4095821cc06fa5a47db33f6bdf0ce7832749775
danixuz <danixuz@icloud.com>
2022-07-08 06:39:57 +0300

    Initial Commit

---
+++ RollingCounterAnimation/RollingCounterAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingCounterAnimationApp.swift
+//  RollingCounterAnimation
+//
+//  Created by Daniel Spalek on 08/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RollingCounterAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
